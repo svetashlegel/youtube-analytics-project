c93b5e9b9723875e276ec2075cf7a5265d8bc146
shlegel <sveta.shlegel@mail.ru>
2023-05-18 15:55:55 +0300

    homework_2

--- src/channel.py
+++ src/channel.py
@@ -11,10 +11,47 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = self.channel["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         res = json.dumps(channel, indent=2, ensure_ascii=False)
         print(res)
+
+    @property
+    def channel_id(self):
+        """Возвращает id канала"""
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API вне класса"""
+        return youtube
+
+    def to_json(self, file_name):
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        data = {
+        'channel_id': self.channel_id,
+        'title': self.title,
+        'description': self.description,
+        'url': self.url,
+        'subscriber_count': self.subscriber_count,
+        'video_count': self.video_count,
+        'view_count': self.view_count
+        }
+        with open(file_name, 'wt') as file:
+            json.dump(data, file)
+
+
+#moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+#print(moscowpython.channel_id)
+#print(Channel.get_service())
+#moscowpython.to_json('example.json')
