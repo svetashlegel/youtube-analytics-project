f0afaa212b7ae3b26693dfdb339dd225ed12cbbc
shlegel <sveta.shlegel@mail.ru>
2023-06-06 11:39:26 +0300

    homework_6

--- src/video.py
+++ src/video.py
@@ -11,19 +11,26 @@ class Video:
     def __init__(self, video_id: str) -> None:
         """Инициализация видео по id, остальные данные получаются по API"""
         self.__video_id = video_id
-        self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=self.__video_id).execute()
-        self.video_title = self.video['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.__video_id).execute()
+            self.video_title = self.video['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except:
+            self.video = None
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """Возвращает название канала"""
         return f'{self.video_title}'
 
 
-class PLVideo (Video):
+class PLVideo(Video):
     """Класс для видео и плейлиста с ютуб"""
 
     def __init__(self, video_id: str, playlist_id: str) -> None:
